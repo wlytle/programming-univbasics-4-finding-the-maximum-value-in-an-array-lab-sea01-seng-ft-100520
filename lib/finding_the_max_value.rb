7d30a60fcb4c72c082cd03f5ba71c422f53fe3f0
Will Lytle <wlytle89@gmail.com>
2020-08-13 17:44:43 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,10 @@
 def find_max_value(array)
   count = 0
+  while count < array.length
+    if !max_value || array[count] > max_value
+      max_value = array[count]
+    end
+    count +=1
+  end
+  return max_value
 end
